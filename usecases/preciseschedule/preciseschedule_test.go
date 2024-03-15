2a8e26024f858dad6864e9f5f5cb173f2c49f60e
quique <enriquesalceda@gmail.com>
2024-03-15 17:56:19 +1100

    errors when group name is not present

--- usecases/preciseschedule/preciseschedule_test.go
+++ usecases/preciseschedule/preciseschedule_test.go
@@ -77,6 +77,29 @@ func TestPreciseSchedule(t *testing.T) {
 		)
 	})
 
+	t.Run("errors when group name is not present", func(t *testing.T) {
+		eventiScheduler := scheduler.New()
+		fakeEventBridge := newFakeEventBridgeSpec("precise-schedule-arn")
+		result := preciseschedule.New(eventiScheduler, fakeEventBridge)(
+			preciseschedule.Input{
+				ClientToken: "bd6dccce-e27a-11ee-87f6-e7571459c4c5",
+				Description: "This a schedule description",
+				Name:        "bd6dccce-e27a-11ee-87f6-e7571459c4c5",
+				At:          "at(2000-01-01T00:00:00)",
+				Target:      "target-arn",
+				DeadLetter:  "dead-letter-arn",
+				RoleArn:     "arn:aws:iam::123456789012:role/schedulerroletoinvoketarget",
+			},
+		)
+
+		require.Error(t, result)
+		require.ErrorContains(
+			t,
+			errors.New("InvalidParameter: 1 validation error(s) found.\n- minimum field size of 1, CreateScheduleInput.GroupName.\n"),
+			result.Error(),
+		)
+	})
+
 	t.Run("errors when target arn is not present", func(t *testing.T) {
 		eventiScheduler := scheduler.New()
 		fakeEventBridge := newFakeEventBridgeSpec("precise-schedule-arn")

315d361a8049d0d0ad2d405dbafc7326069a7228
quique <enriquesalceda@gmail.com>
2024-03-15 15:17:44 +1100

    build base schedule input

--- modules/scheduler/scheduler.go
+++ modules/scheduler/scheduler.go
@@ -1,15 +1,17 @@
 package scheduler
 
-import (
-	"github.com/aws/aws-sdk-go/aws"
-	awsscheduler "github.com/aws/aws-sdk-go/service/scheduler"
-)
+type BaseScheduleInput struct {
+	ClientToken string
+	Description string
+	GroupName   string
+	Name        string
+}
 
-func New(clientToken string, description string, groupName string, name string) *awsscheduler.CreateScheduleInput {
-	return &awsscheduler.CreateScheduleInput{
-		ClientToken: aws.String(clientToken),
-		Description: aws.String(description),
-		GroupName:   aws.String(groupName),
-		Name:        aws.String(name),
+func New(clientToken string, description string, groupName string, name string) *BaseScheduleInput {
+	return &BaseScheduleInput{
+		ClientToken: clientToken,
+		Description: description,
+		GroupName:   groupName,
+		Name:        name,
 	}
 }

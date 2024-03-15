172b122d7789544aa1ebc336517afca6b477274d
quique <enriquesalceda@gmail.com>
2024-03-15 15:05:19 +1100

    NewScheduler

--- modules/scheduler/scheduler.go
+++ modules/scheduler/scheduler.go
@@ -1,1 +1,15 @@
 package scheduler
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	awsscheduler "github.com/aws/aws-sdk-go/service/scheduler"
+)
+
+func New(clientToken string, description string, groupName string, name string) *awsscheduler.CreateScheduleInput {
+	return &awsscheduler.CreateScheduleInput{
+		ClientToken: aws.String(clientToken),
+		Description: aws.String(description),
+		GroupName:   aws.String(groupName),
+		Name:        aws.String(name),
+	}
+}
--- modules/scheduler/scheduler_test.go
+++ modules/scheduler/scheduler_test.go
@@ -1,1 +1,31 @@
 package scheduler_test
+
+import (
+	"eventi/modules/scheduler"
+	"github.com/aws/aws-sdk-go/aws"
+	awsscheduler "github.com/aws/aws-sdk-go/service/scheduler"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestScheduler(t *testing.T) {
+	t.Run("NewScheduler", func(t *testing.T) {
+		schdl := scheduler.New(
+			"bd6dccce-e27a-11ee-87f6-e7571459c4c5",
+			"This a schedule description",
+			"my-group",
+			"bd6dccce-e27a-11ee-87f6-e7571459c4c5",
+		)
+
+		require.Equal(
+			t,
+			&awsscheduler.CreateScheduleInput{
+				ClientToken: aws.String("bd6dccce-e27a-11ee-87f6-e7571459c4c5"),
+				Description: aws.String("This a schedule description"),
+				GroupName:   aws.String("my-group"),
+				Name:        aws.String("bd6dccce-e27a-11ee-87f6-e7571459c4c5"),
+			},
+			schdl,
+		)
+	})
+}

6a281460caf4486bff7f7b364568f67f14921a61
quique <enriquesalceda@gmail.com>
2024-03-15 17:59:18 +1100

    errors when target arn is not present

--- usecases/preciseschedule/preciseschedule_test.go
+++ usecases/preciseschedule/preciseschedule_test.go
@@ -145,6 +145,29 @@ func TestPreciseSchedule(t *testing.T) {
 			result.Error(),
 		)
 	})
+
+	t.Run("errors when target arn is not present", func(t *testing.T) {
+		eventiScheduler := scheduler.New()
+		fakeEventBridge := newFakeEventBridgeSpec("precise-schedule-arn")
+		result := preciseschedule.New(eventiScheduler, fakeEventBridge)(
+			preciseschedule.Input{
+				ClientToken: "bd6dccce-e27a-11ee-87f6-e7571459c4c5",
+				Description: "This a schedule description",
+				GroupName:   "my-group",
+				Name:        "bd6dccce-e27a-11ee-87f6-e7571459c4c5",
+				At:          "at(2000-01-01T00:00:00)",
+				DeadLetter:  "dead-letter-arn",
+				RoleArn:     "arn:aws:iam::123456789012:role/schedulerroletoinvoketarget",
+			},
+		)
+
+		require.Error(t, result)
+		require.ErrorContains(
+			t,
+			errors.New("InvalidParameter: 1 validation error(s) found.\n- minimum field size of 1, CreateScheduleInput.Target.Arn.\n"),
+			result.Error(),
+		)
+	})
 }
 
 type fakeEventBridgeClient struct {
